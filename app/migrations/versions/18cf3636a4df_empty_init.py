66b7b0799aca37fe7b5a63a6e21580e6e22608d6
koskei-kipkoech <patrickwayy@gmail.com>
2024-12-12 11:48:57 +0300

    creates app

---
+++ app/migrations/versions/18cf3636a4df_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 18cf3636a4df
+Revises: 
+Create Date: 2024-12-12 11:42:45.733642
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '18cf3636a4df'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
